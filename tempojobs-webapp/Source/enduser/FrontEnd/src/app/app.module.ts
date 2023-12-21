76f27d43c23c6a43c439a0fd902edf778eff21d5
Hoang Ta <taduyhoang10@gmail.com>
2023-12-21 21:01:54 +0700

    change api key

--- tempojobs-webapp/Source/enduser/FrontEnd/src/app/app.module.ts
+++ tempojobs-webapp/Source/enduser/FrontEnd/src/app/app.module.ts
@@ -39,7 +39,7 @@ import {StorageModule} from '@angular/fire/storage'
     NbToastrModule.forRoot(),
     QuillModule.forRoot(),
     NbChatModule.forRoot({
-      messageGoogleMapKey: 'AIzaSyDPOyzngYDXtIRdJTDFwSV02xNIDLEXgHI',
+      messageGoogleMapKey: 'AIzaSyC6n653gO_6ToPPi3NnPmGkSYMk4iv1MBA',
     }),
     NbLayoutModule,
     NbEvaIconsModule,
