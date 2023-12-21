fcbc410dfb7dce5c79d8d986e21b7659b724f1ca
Hoang Ta <taduyhoang10@gmail.com>
2023-12-22 00:34:22 +0700

    add google location and province pipe in work detail

---
+++ tempojobs-webapp/Source/enduser/FrontEnd/src/app/modules/home/work-management/work-detail/province-convert.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ProvinceConvertPipe } from './province-convert.pipe';
+
+describe('ProvinceConvertPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ProvinceConvertPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
